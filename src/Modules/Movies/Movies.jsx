7e3e62a6da351fca50e164fdc3a88376b4388553
leo-moon <49075905+leo-moon@users.noreply.github.com>
2023-02-17 15:54:21 +0200

    start

---
+++ src/Modules/Movies/Movies.jsx
@@ -0,0 +1,11 @@
+import styles from './movies.module.css'
+
+const Movies = () => {
+    return (
+        <>
+        <p></p>
+        </>
+    )
+}
+
+export default Movies;
---
+++ src/shared/fitchApi.js
@@ -0,0 +1,2 @@
+const apiKey = '1a6a0e252669dea132492bf96fa96565'
+const MAINURL = 'https://api.themoviedb.org/3/movie/76341?api_key=<';
