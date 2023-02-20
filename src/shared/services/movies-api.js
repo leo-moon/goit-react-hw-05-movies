5fc740d67da06b95914b25e601332e115a1f27ca
leo-moon <49075905+leo-moon@users.noreply.github.com>
2023-02-21 00:42:33 +0200

    0.42

--- src/shared/services/movies-api.js
+++ src/shared/services/movies-api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const mainURL = 'https://api.themoviedb.org/3/';
-const media_type_values = ['all', 'movie', 'TV', 'person'];
-const time_window_values = ['day', 'week'];
+// const media_type_values = ['all', 'movie', 'TV', 'person'];
+// const time_window_values = ['day', 'week'];
 const api_KEY = '?api_key=1a6a0e252669dea132492bf96fa96565';
-const media_type = media_type_values[1];
-const time_window = time_window_values[0];
+// const media_type = media_type_values[1];
+// const time_window = time_window_values[0];
 const trendingURL =
   'https://api.themoviedb.org/3/trending/movie/day?api_key=1a6a0e252669dea132492bf96fa96565';
   // const trendingURL =
