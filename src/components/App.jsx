a2e109f94d3991f66c19a596cc0c4bbd3a9eec1d
leo-moon <49075905+leo-moon@users.noreply.github.com>
2023-02-21 14:28:11 +0200

    fin

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,13 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import Navbar from '../Modules/Navbar/Navbar';
-import HomePage from '../pages/HomePage/HomePage';
-import MoviePage from '../pages/MoviePage/MoviePage';
-import TVPage from 'pages/TV/Tv';
-import SingleMoviePage from '../pages/SingleMoviePage/SingleMoviePage';
-import CastPage from 'pages/CastPage/CastPage';
-import ReviewPage from 'pages/ReviewsPage/ReviewsPage';
-import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
-// import Movies from '../Modules/Movies/Movies';
+import UserRoutes from './UserRoutes';
 
 export function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/movie" element={<MoviePage />} />
-        <Route path="/tv" element={<TVPage />} />
-        <Route path="/:media_type/:id" element={<SingleMoviePage />}>
-          <Route path="cast" element={<CastPage />} />
-          <Route path="reviews" element={<ReviewPage />} />
-        </Route>
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <UserRoutes />
     </BrowserRouter>
   );
 }
---
+++ src/components/UserRoutes.jsx
@@ -0,0 +1,29 @@
+import { lazy, Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
+const MoviePage = lazy(() => import('../pages/MoviePage/MoviePage'));
+const TVPage = lazy(() => import('pages/TV/Tv'));
+const SingleMoviePage = lazy(() => import('../pages/SingleMoviePage/SingleMoviePage'));
+const CastPage = lazy(() => import('pages/CastPage/CastPage'));
+const ReviewPage = lazy(() => import('pages/ReviewsPage/ReviewsPage'));
+const NotFoundPage = lazy(() => import('../pages/NotFoundPage/NotFoundPage'));
+
+const UserRoutes = () => {
+  return (
+    <Suspense fallback={<p>...loading</p>}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movie" element={<MoviePage />} />
+        <Route path="/tv" element={<TVPage />} />
+        <Route path="/:media_type/:id" element={<SingleMoviePage />}>
+          <Route path="cast" element={<CastPage />} />
+          <Route path="reviews" element={<ReviewPage />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Suspense>
+  );
+};
+
+export default UserRoutes;
